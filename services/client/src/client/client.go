ee50ccbf858306c25c3ce49b4b537988ed83ecde
agustinghersi <agusghersi@gmail.com>
2026-08-28 21:32:20 -0300

    Agrego lectura por linea en el cliente y envio al server

--- services/client/src/client/client.go
+++ services/client/src/client/client.go
@@ -3,6 +3,8 @@ package client
 import (
 	"net"
 	"time"
+	"os"
+	"bufio"
 
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/logger"
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/safe_socket"
@@ -19,6 +21,7 @@ type ClientConfig struct {
 	ServerHost string
 	ServerPort string
 	AgencyId   string
+	InputFile  string
 }
 
 type Client struct {
@@ -58,11 +61,54 @@ func connectToServer(host, port string) (net.Conn, error) {
 	return conn, err
 }
 
+func (client *Client) readInputFile() error {
+	// Abro el archivo definido como INPUT_FILE en docker-compose.yaml
+	file, err := os.Open(client.config.InputFile)
+	if err != nil {
+		logger.Error("open-input-file", logger.Fail, "input-file", client.config.InputFile)
+		return err
+	}
+
+	defer file.Close() // El archivo lo cierro ya porque me queda la info en file
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		
+		line := scanner.Text() // Lee de a 1 linea
+
+		// Ver si despues se requiere trabajar la linea para que no quede texto
+		//De momento solo me copio lo que estaba en el esquleto para mandar las lineas 1 a 1
+		//readder := csv.NewReader(file)
+
+		if err := safe_socket.SendAll(client.conn, []byte(line)); err != nil {
+			logger.Error("send-message", logger.Fail)
+			return err
+		}
+
+		responseBuffer, err := safe_socket.RecvAll(client.conn, ECHO_CLIENT_BUFFER_SIZE)
+		if err != nil {
+			logger.Error("recv-response", logger.Fail)
+			return err
+		}
+
+		if string(responseBuffer) != line {
+			logger.Error("check-response", logger.Fail)
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+
+
 func (client *Client) Run() error {
 	const mainAction = "test-echo-server"
 	defer client.conn.Close()
 
-	for messageId := range ECHO_CLIENT_MESSAGE_AMOUNT {
+	/* for messageId := range ECHO_CLIENT_MESSAGE_AMOUNT {
 		messageArgs := []any{"agency-id", client.config.AgencyId, "message-id", messageId}
 		logger.Info(mainAction, logger.InProgress, messageArgs...)
 
@@ -85,6 +131,10 @@ func (client *Client) Run() error {
 		}
 
 		time.Sleep(ECHO_CLIENT_MESSAGE_DELAY_MS * time.Millisecond)
+	} */
+	if err := client.readInputFile(); err != nil {
+		logger.Error("read-input-file", logger.Fail)
+		return err
 	}
 	logger.Info(mainAction, logger.Success, "agency-id", client.config.AgencyId)
 
